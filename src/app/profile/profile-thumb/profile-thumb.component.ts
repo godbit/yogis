85691d9344f8e70161e0d4a6e314c00636e912e4
Emil Plynning <emil.plynning@gmail.com>
2019-02-05 20:47:32 +0100

    Generate empty, not yet used profile components

---
+++ src/app/profile/profile-thumb/profile-thumb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-thumb',
+  templateUrl: './profile-thumb.component.html',
+  styleUrls: ['./profile-thumb.component.css']
+})
+export class ProfileThumbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileContainerComponent } from './profile-container/profile-container.component';
+import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
+import { ProfileThumbComponent } from './profile-thumb/profile-thumb.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
 
 @NgModule({
-  declarations: [ProfileContainerComponent],
+  declarations: [ProfileContainerComponent, ProfilePictureComponent, ProfileThumbComponent, ProfileDetailsComponent],
   imports: [
     CommonModule
   ],
