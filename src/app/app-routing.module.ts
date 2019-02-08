08f978c6184c5b7426071a52c0be7c90fa04c5fe
Emil Plynning <emil.plynning@gmail.com>
2019-02-08 22:38:13 +0100

    Add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileContainerComponent } from './profile/profile-container/profile-container.component';
+import { ProfileThumbComponent } from './profile/profile-thumb/profile-thumb.component';
+import { ProfilePictureComponent } from './profile/profile-picture/profile-picture.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile', component: ProfileContainerComponent
+  },
+  {
+    path: 'thumb', component: ProfileThumbComponent
+  },
+  {
+    path: 'pic', component: ProfilePictureComponent
+  },
+  {
+    path: '', redirectTo: '/profile', pathMatch: 'full'
+  },
+  {
+    path: '**', component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// Avoid having to specify routing components in serveral locations by adding them to routingComponents.
+// export const rountingComponents = [ProfileContainerComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileModule } from './profile/profile.module';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavModule } from './nav/nav.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ProfileModule,
+    NavModule,
     HttpClientModule,
     BrowserAnimationsModule
   ],
