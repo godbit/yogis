b76899828da572784e7735a02a2bac808b217169
Emil Plynning <emil.plynning@gmail.com>
2019-02-17 13:56:08 +0100

    Add nav menu for mobile devices

--- src/app/nav/nav.module.ts
+++ src/app/nav/nav.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule,
+    MatIconModule
   ],
   exports: [
     NavbarComponent
